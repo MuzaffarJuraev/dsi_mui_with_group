7a4d1ad47cf88bbb1393383e6d75ca45dd670fb3
MuzaffarJuraev <juraev_m2000@mail.ru>
2022-02-15 15:33:41 +0500

    start of project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      {/*navbar start */}
+
+      {/*navbar end */}
+
+      {/* Information technology  start*/}
+
+      {/* Information technology end */}
+
+      {/* Dwidasa Samsara start */}
+
+      {/* Dwidasa Samsara end */}
+
+      {/* Product and Service start */}
+
+      {/* Product and Service end */}
+
+      {/* Contact start */}
+
+      {/* Contact  end */}
+
+      {/* Footer start */}
+
+      {/* Footer end */}
+    </div>
+  );
 }
 
 export default App;
